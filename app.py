5ca79da931d30c230836f4fefc5fbf8855e10d70
Nicklas Thegerström <nicklas.thegerstrom@gmail.com>
2026-06-28 11:41:14 +0200

    new model

--- app.py
+++ app.py
@@ -45,7 +45,7 @@ def load_models():
         "models/mustache_detector_3.keras"
     )
     epic_model = tf.keras.models.load_model(
-        "models/epic_detector_3class_3.keras"
+        "models/epic_detector.keras"
     )
     return mustache_model, epic_model
 
